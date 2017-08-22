bf68d4e4f02b63a16e357d875b72116a7e25a649
SHUBHAMTRIPATH <31066199+SHUBHAMTRIPATH@users.noreply.github.com>
2017-08-22 15:13:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/action-one', function (req, res) {
+app.get('/ui/action-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'action-one.html'));
 });
 
